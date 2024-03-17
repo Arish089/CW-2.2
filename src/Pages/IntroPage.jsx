21cb158eeedeeef05111222920b781db62442630
Arish089 <152006719+Arish089@users.noreply.github.com>
2024-03-17 20:19:49 +0530

    done

--- src/Pages/IntroPage.jsx
+++ src/Pages/IntroPage.jsx
@@ -28,15 +28,15 @@ const IntroPage = () => {
     <Box display='flex' minH='87vh' justifyContent='center' alignItems='center'
      >
         
-        <Flex w='50%' minH={200} justifyContent='space-between' direction={{base:'column',md:'row'}} fontWeight='semibold' fontSize='x-large' h={{base:250,md:'auto'}}>
-        <Box className='anonym' boxShadow='Dark lg' bgGradient='linear(to-b, #FF2400, black)' w={{base:'auto',md:'40%'}} color='white' display='flex' h={{base:100,md:'inherit'}}
+        <Flex w='50%' minH={200} justifyContent='space-between' direction={{base:'column',md:'row'}} fontWeight='semibold' fontSize={{base:'large',md:'larger',lg:'x-large'}} h={{base:200,md:'auto'}}>
+        <Box className='anonym' boxShadow='Dark lg' bgGradient='linear(to-b, #FF2400, black)' w={{base:'auto',md:'40%'}} color='white' display='flex' h={{base:'80px',md:'inherit'}}
         alignItems='center' rounded='lg' justifyContent='center' textAlign='center' _hover={{bgColor:'#2D3748',bgGradient: 'linear(to-b ,black, #FF2400)'}} cursor='pointer'
          >
             <Link onClick={signInAnonymous} >Explore as Guest</Link>
             </Box>
         <Spacer />
         <Box className='SignLog' boxShadow='Dark lg' bgGradient='linear(to-b, #FF2400, black)' w={{base:'auto',md:'40%'}} cursor='pointer' justifyContent='center' color='white' rounded='lg'
-          _hover={{bgColor:'#2D3748',bgGradient: 'linear(to-b ,black, #FF2400)'}} h={{base:100,md:'inherit'}} display='flex' alignItems='center'>
+          _hover={{bgColor:'#2D3748',bgGradient: 'linear(to-b ,black, #FF2400)'}} h={{base:'80px',md:'inherit'}} display='flex' alignItems='center'>
             <Link to='/login' > Login</Link>
             /
         <Link to='/signup' >Signup</Link>
