d168db8944dafe6bd87178448237fa69efa12167
Arish089 <152006719+Arish089@users.noreply.github.com>
2024-07-06 02:20:52 +0530

    last few touches

--- src/Pages/Favorite.jsx
+++ src/Pages/Favorite.jsx
@@ -27,7 +27,7 @@ const Favorite = () => {
     listItems()
   },[])
   return (
-    <Box color='white' p={10} bg='black'>
+    <Box color='white' p={10} bg='black' minH='60vh'>
       <Center>
         <Heading color='white' fontSize={36} >Favorite</Heading></Center>
       <Flex py={4} fontSize={28} w={{base:'100%',sm:'85%',md:'75%',lg:'50%'}} m='auto' overflowY={'scroll'}>
